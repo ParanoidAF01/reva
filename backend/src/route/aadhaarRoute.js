f5dc5524fcfaa83a98f4731d5d9d65f3273ee13e
Ishaan Minocha <minochaishaan2003@gmail.com>
2025-08-08 01:25:23 +0530

    middleware to otp routes added

--- backend/src/route/aadhaarRoute.js
+++ backend/src/route/aadhaarRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { generateAadhaarOtp, submitAadhaarOtp } from "../controllers/aadhaarController.js";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
 
 const aadhaarRoute = express.Router();
 
-aadhaarRoute.post("/generate-otp", generateAadhaarOtp);
-aadhaarRoute.post("/submit-otp", submitAadhaarOtp);
+aadhaarRoute.post("/generate-otp", verifyJWT, generateAadhaarOtp);
+aadhaarRoute.post("/submit-otp", verifyJWT, submitAadhaarOtp);
 
 export default aadhaarRoute;
